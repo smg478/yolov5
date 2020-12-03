73b37e0223991f1eeaf77f8404684499683f64e0
Shaikat Galib <smg478@mst.edu>
2020-12-03 14:00:21 -0600

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,10 +1,7 @@
 import argparse
-import os
-import platform
-import shutil
 import time
 from pathlib import Path
-import pandas as pd
+
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
@@ -12,9 +9,9 @@ from numpy import random
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
-from utils.general import (
-    check_img_size, non_max_suppression, apply_classifier, scale_coords,
-    xyxy2xywh, plot_one_box, strip_optimizer, set_logging)
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
+    strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
